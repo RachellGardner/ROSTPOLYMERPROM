f9751b1026938d6806c7088fd1a33f22598e533c
sky1768205 <Tinchurina.dinara@mail.ru>
2025-06-27 16:53:58 +0300

    1

---
+++ Python_kt_4-main/Python_kt_4-main/Python_KT-4-main/Python_KT-4-main/blog/blogs/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import BlogPost, Entry
+
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ['text']
+        labels = {'text': ''}
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': 'Entry:'}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
